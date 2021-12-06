b3429138bc7a151dd7e84038873eecb1b1a20f94
sweeneygood <sweeneygood@gmail.com>
2021-12-05 20:15:19 -0600

    worked on filter

--- static/js/app.js
+++ static/js/app.js
@@ -3,15 +3,49 @@ var tableData = data;
 
 var tbody = d3.select("tbody");
 
+// Select the button
+var button = d3.select("#button");
 
-data.forEach(function(UFO) {
-    console.log(UFO);
-    var row = tbody.append("tr");
+// Select the form
+var form = d3.select("#form");
 
-    Object.entries(UFO).forEach(function([key, value]) {
-        console.log(key, value);
-    
-    var cell = row.append("td");        
-    cell.text(value);
-  });
-});
+// Create event handlers 
+button.on("click", runFilter);
+form.on("submit", runFilter);
+
+displayTable(tableData); 
+
+function runFilter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datefilter1");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+
+    //var filteredData = tableData.filter(tableData => Date(tableData.datetime) === inputValue);
+
+    Object.entries(data).forEach(([key, value]) => {
+         filteredData = filteredData.filter(row => row[key] === value);
+       });
+
+    console.log(filteredData);
+    displayTable(filteredData); 
+}
+
+
+function displayTable(tableData) {
+    tableData.forEach(function (UFO) {
+        var row = tbody.append("tr");
+
+        Object.entries(UFO).forEach(function ([key, value]) {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+}
