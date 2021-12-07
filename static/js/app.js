59a06e716140673fee8a0da7a000b90c25725580
sweeneygood <sweeneygood@gmail.com>
2021-12-06 21:31:05 -0600

    updated filter code

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,7 @@
-// from data.js
+// from data.js, put the list of dicts into tableData
 var tableData = data;
 
+// Select the table 
 var tbody = d3.select("tbody");
 
 // Select the button
@@ -26,24 +27,46 @@ function runFilter() {
     // Get the value property of the input element
     var inputValue = inputElement.property("value");
 
-    console.log(inputValue);
 
-    //var filteredData = tableData.filter(tableData => Date(tableData.datetime) === inputValue);
+   // d1 = new Date(document.getElementById("date1").value);
 
-    Object.entries(data).forEach(([key, value]) => {
-         filteredData = filteredData.filter(row => row[key] === value);
-       });
+    console.log("inputValue");
+    console.log(formatDate(inputValue));
+
+    var filteredData = tableData.filter(o => o.datetime === formatDate(inputValue));
 
     console.log(filteredData);
+
+    // Clear original table
+    tbody.html("");
+
+    // Call the displayTable function for the filtered dataset 
     displayTable(filteredData); 
+
 }
 
 
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+ 
+
+
 function displayTable(tableData) {
-    tableData.forEach(function (UFO) {
+    tableData.forEach(function (UFOdata) {
         var row = tbody.append("tr");
 
-        Object.entries(UFO).forEach(function ([key, value]) {
+        Object.entries(UFOdata).forEach(function ([key, value]) {
             var cell = row.append("td");
             cell.text(value);
         });
